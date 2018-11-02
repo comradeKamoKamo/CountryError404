c76089c4764f33ae2fe090c9228b1c8cf42d7d01
KamoKamo <tamutamuphone@gmail.com>
2018-11-02 20:42:32 +0900

    ツイートエンジン実装

--- engine/engine.py
+++ engine/engine.py
@@ -1,5 +1,7 @@
 import tweepy
 import os
+from datetime import datetime
+from pathlib import Path
 
 def main():
     #OAuth
@@ -10,9 +12,27 @@ def main():
     auth = tweepy.OAuthHandler(api_key,api_secret)
     auth.set_access_token(access_token,access_token_secret)
     api = tweepy.API(auth)
-    
-    api.update_status("{0}".format(os.times()))
 
+    #Call Tasks
+    tweet_articles(api,"tweets/id_list.txt")
+
+def tweet_articles(api,list_path):
+    # リストから順番にツイートする
+    with Path(list_path).open("r",encoding="utf-8") as f:
+        count = 0
+        if Path("engine/count.dat").exists():
+          with Path("engine/count.dat").open("r",encoding="utf-8") as c:
+                count = int(c.readline())
+        lines = f.readlines()
+        if len(lines) <= count:
+            count = 0
+        text = lines[count][0:-1]
+    with Path("engine/count.dat").open("w",encoding="utf-8") as c:
+                c.write("{0}".format(count+1))
+    # Tweet    
+    api.update_status(text)
+    return
+    
 
 if __name__=="__main__":
     main()
