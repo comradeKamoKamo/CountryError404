547175d71805f8285b3c01268652d25a4e595f6f
KamoKamo <tamutamuphone@gmail.com>
2020-08-24 00:29:06 +0900

    fix list_timeline fails with 404.

--- engine/engine.py
+++ engine/engine.py
@@ -6,96 +6,101 @@ from pathlib import Path
 import pickle
 import boto3
 
+
 def main():
 
-    #時刻制御
+    # 時刻制御
     if datetime.now().hour > 22 or datetime.now().hour < 7:
         exit()
 
-    #S3 Load
+    # S3 Load
     Path("tmp").mkdir(exist_ok=True)
-    s3 , bucket =  save_from_s3(s3_name = os.environ.get("S3_BUCKET"))
+    s3, bucket = save_from_s3(s3_name=os.environ.get("S3_BUCKET"))
 
-    #OAuth
+    # OAuth
     api_key = os.environ.get("API_KEY")
     api_secret = os.environ.get("API_SECRET")
     access_token = os.environ.get("ACCESS_TOKEN")
     access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
-    auth = tweepy.OAuthHandler(api_key,api_secret)
-    auth.set_access_token(access_token,access_token_secret)
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    #Call Tasks
-    tweet_articles(api,"tweets/id_list.txt")
+    # Call Tasks
+    tweet_articles(api, "tweets/id_list.txt")
     follow_and_remove(api)
     tweet_news(api)
-    
-    #S3 Save
-    save_to_s3(s3,bucket)
 
-def save_to_s3(s3,bucket):
+    # S3 Save
+    save_to_s3(s3, bucket)
+
+
+def save_to_s3(s3, bucket):
     bucket.upload_file('tmp/count.dat', 'count.dat')
     if Path("tmp/protected_users.pickle").exists():
-        bucket.upload_file('tmp/protected_users.pickle', 'protected_users.pickle')
+        bucket.upload_file('tmp/protected_users.pickle',
+                           'protected_users.pickle')
     return
+
 
 def save_from_s3(s3_name):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(s3_name)
     try:
         bucket.download_file("count.dat", "tmp/count.dat")
-        bucket.download_file("protected_users.pickle", "tmp/protected_users.pickle")
+        bucket.download_file("protected_users.pickle",
+                             "tmp/protected_users.pickle")
     except:
         print("Download from S3 failed.")
-    return s3 , bucket
-        
+    return s3, bucket
 
-def tweet_articles(api,list_path):
+
+def tweet_articles(api, list_path):
     # リストから順番にツイートする
-    with Path(list_path).open("r",encoding="utf-8") as f:
+    with Path(list_path).open("r", encoding="utf-8") as f:
         count = 0
         if Path("tmp/count.dat").exists():
-          with Path("tmp/count.dat").open("r",encoding="utf-8") as c:
+            with Path("tmp/count.dat").open("r", encoding="utf-8") as c:
                 count = int(c.readline())
         lines = f.readlines()
         if len(lines) <= count:
             count = 0
-        text = lines[count].replace("\n","")
+        text = lines[count].replace("\n", "")
         if len(text) == 0:
             print("Error: id_list has an empty line.")
-    # Tweet    
+    # Tweet
     try:
         api.update_status(text)
         print("Tweet:" + text)
     except tweepy.TweepError as e:
-        if e.api_code==187:
-            #ツイートの重複
+        if e.api_code == 187:
+            # ツイートの重複
             print("Error: 187 - Status is a duplicate. count is " + count)
             count = count + 1
         else:
             print("Error: " + e.api_code)
-    with Path("tmp/count.dat").open("w",encoding="utf-8") as c:
+    with Path("tmp/count.dat").open("w", encoding="utf-8") as c:
         c.write("{0}".format(count+1))
     return
 
-def tweet_news(api):
-    #単語取得
-    keywords = []
-    with Path("engine/keywords.txt").open("r",encoding="utf-8") as f:
-        for line in f.readlines():
-            if line[0:1]=="#" or line[0:1]=="\n":
-                continue
-            keywords.append(list(line[0:-1].split(","))) 
 
-    #ニュースメディアリストから最新900件を取得。
-    for tweet in tweepy.Cursor(api.list_timeline,"CountryError404",
-    "Media",tweet_mode="extended").items(900):
+def tweet_news(api):
+    # 単語取得
+    keywords = []
+    with Path("engine/keywords.txt").open("r", encoding="utf-8") as f:
+        for line in f.readlines():
+            if line[0:1] == "#" or line[0:1] == "\n":
+                continue
+            keywords.append(list(line[0:-1].split(",")))
+
+    # ニュースメディアリストから最新900件を取得。
+    for tweet in api.list_timeline(list_id="1059060887087017985", tweet_mode="extended", count=900):
         text = get_display_text(tweet)
         retweeted = False
         for wset in keywords:
             if wset[0] in text:
                 if len(wset) > 1:
-                    for i in range(1,len(wset)-1):
+                    for i in range(1, len(wset)-1):
                         if wset[i] in text:
                             if not tweet.retweeted:
                                 api.retweet(tweet.id)
@@ -107,14 +112,17 @@ def tweet_news(api):
                         api.retweet(tweet.id)
                         retweeted = True
                         print(text)
-            if retweeted: break
-        
+            if retweeted:
+                break
+
+
 def get_display_text(tweet):
-    s , e = tuple(tweet.display_text_range)
+    s, e = tuple(tweet.display_text_range)
     return tweet.full_text[s:e]
 
+
 def follow_and_remove(api):
-    #24時間で1000回まで。1時間100回。
+    # 24時間で1000回まで。1時間100回。
     users = []
     protected_users = []
     followers_id = []
@@ -124,20 +132,20 @@ def follow_and_remove(api):
     for friend_id in tweepy.Cursor(api.friends_ids).items():
         friends_id.append(friend_id)
 
-    if len(followers_id) > 0:    
+    if len(followers_id) > 0:
         if Path("tmp/protected_users.pickle").exists():
             with Path("tmp/protected_users.pickle").open("rb") as f:
                 protected_users = pickle.load(f)
-        #LOOKUPで100ごと取得
+        # LOOKUPで100ごと取得
         for i in range(0, len(followers_id), 100):
             for follower in api.lookup_users(user_ids=followers_id[i:i+100]):
                 if not follower.following:
                     if follower.protected:
-                        #鍵垢の場合は1回だけフォローリクエストを送る。
+                        # 鍵垢の場合は1回だけフォローリクエストを送る。
                         if not follower.id in protected_users:
                             if not follower.follow_request_sent:
                                 users.append(follower)
-                                protected_users.append(follower.id)     
+                                protected_users.append(follower.id)
                     else:
                         users.append(follower)
         for i in range(100):
@@ -145,24 +153,24 @@ def follow_and_remove(api):
                 break
             users.pop().follow()
 
-        #鍵垢リストの確認
+        # 鍵垢リストの確認
         for user_id in protected_users:
             try:
-                #フォロー外されている
+                # フォロー外されている
                 if not user_id in followers_id:
                     protected_users.remove(user_id)
-                #フォローしている
+                # フォローしている
                 elif api.get_user(user_id).following:
                     protected_users.remove(user_id)
             except tweepy.TweepError:
                 # tweepy.error.TweepError: [{'code': 50, 'message': 'User not found.'}]
                 protected_users.remove(user_id)
-        
-        #鍵垢リスト保存
-        with Path("tmp/protected_users.pickle").open("wb") as f:
-            pickle.dump(protected_users,f)
 
-    #リムーブ
+        # 鍵垢リスト保存
+        with Path("tmp/protected_users.pickle").open("wb") as f:
+            pickle.dump(protected_users, f)
+
+    # リムーブ
     removers_id = []
     for friend_id in friends_id:
         if not friend_id in followers_id:
@@ -171,8 +179,7 @@ def follow_and_remove(api):
         if len(removers_id) <= i:
             break
         api.destroy_friendship(removers_id.pop())
-    
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
